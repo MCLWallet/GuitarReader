3bf245862952911c2a5c86441a58edabd568f924
Marcell Lanczos <MCL@Marcells-MBP.home>
2016-06-05 22:26:02 +0200

    dlls added

--- Assets/PowerChordsFinder.cs
+++ Assets/PowerChordsFinder.cs
@@ -2,23 +2,36 @@ using UnityEngine;
 using System.Collections;
 
 using MidiJack;
+using Sanford.Multimedia.Midi;
 
 public class PowerChordsFinder : MonoBehaviour
 {
+	/*
+	InputDevice inDevice = new InputDevice(0);
+	ChannelStopper stopper = new ChannelStopper();
+	*/
 
 
 
 	// Use this for initialization
 	void Start ()
 	{
+		//Debug.Log(inDevice.DeviceID);
+		OutputDevice output = new OutputDevice(0);
+		ChannelMessage message = new ChannelMessage(ChannelCommand.NoteOn, 0, 60, 100);
 
+		output.Send (message);
+
+		System.Threading.Thread.Sleep(100);
+
+		message = new ChannelMessage(ChannelCommand.NoteOff, 0, 60, 100);
+		output.Send(message);
 
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-
 	
 	}
 }
